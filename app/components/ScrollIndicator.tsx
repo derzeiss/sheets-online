db1c4b04d672d4dae84fd0e2d1d9fdfd675aabd9
derzeiss <derzeiss@users.noreply.github.com>
2025-04-25 16:10:02 +0200

    Add pretty script & prettify app/

--- app/components/ScrollIndicator.tsx
+++ app/components/ScrollIndicator.tsx
@@ -4,34 +4,6 @@ interface Props {
   targetSel?: string;
 }
 
-// DISCLAIMER: this hook is untested
-const useYScrollProgress = (targetSel: string) => {
-  const [scalePerc, setScalePerc] = useState(0);
-
-  useEffect(() => {
-    const $el = document.querySelector(targetSel) as HTMLElement | null;
-    if (!$el) return;
-
-    const updateScrollPerc = (ev: Event) => {
-      const minPixel = $el.offsetTop;
-      const maxPixel = minPixel + $el.scrollHeight;
-      const value = document.body.scrollTop;
-
-      // respect bounds of element
-      let percent = (value - minPixel) / (maxPixel - minPixel);
-      percent = Math.min(1, Math.max(percent, 0)) * 100;
-
-      setScalePerc(percent);
-    };
-
-    window.addEventListener('scroll', updateScrollPerc);
-
-    () => window.removeEventListener('scroll', updateScrollPerc);
-  }, []);
-
-  return scalePerc;
-};
-
 const useXScrollProgress = (targetSel: string) => {
   const [scalePerc, setScalePerc] = useState(0);
 
@@ -64,7 +36,7 @@ export const ScrollIndicator: FC<Props> = ({ targetSel = 'html' }) => {
 
   return (
     <div
-      className="fixed left-0 top-0 z-20 h-2 w-full origin-left bg-blue-300"
+      className="fixed top-0 left-0 z-20 h-2 w-full origin-left bg-blue-300"
       style={{ transform: `scaleX(${scalePerc}%)` }}
     />
   );
