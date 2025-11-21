62cda9ebabce0e642b147226e8b2a4df6eb932de
derzeiss <derzeiss@users.noreply.github.com>
2025-11-21 16:02:38 +0100

    Inline dependencies for migration script

--- prisma/migrations/20251121134058_add_setlist_slug/migration_update_slugs.ts
+++ prisma/migrations/20251121134058_add_setlist_slug/migration_update_slugs.ts
@@ -1,6 +1,4 @@
-import { prisma } from '../../../app/domain/prisma';
-import { findNextUniqueSlug } from '../../../app/domain/utils/findNextUniqueSlug.server';
-import { toSlug } from '../../../app/domain/utils/toSlug';
+import { PrismaClient } from '@prisma/client';
 
 async function main() {
   console.log('Fetching setlists');
@@ -24,5 +22,80 @@ async function main() {
 
   console.log(`Successfully updated ${setlists.length} setlists.`);
 }
+
+// ---- DEPS ----
+
+// copied from '~/domain/prisma'
+const prisma = new PrismaClient();
+
+// copied from '~/domain/utils/findNextUniqueSlug.server'
+const findNextUniqueSlug = async <T extends Function>(
+  findUnique: T,
+  baseSlug: string,
+  suffix = 0,
+): Promise<string> => {
+  // don't allow 'new' as we use it for creating a new blank setlist
+  if (suffix === 0 && baseSlug === 'new') {
+    return findNextUniqueSlug(findUnique, baseSlug, suffix + 1);
+  }
+  let slug = suffix === 0 ? baseSlug : `${baseSlug}-${suffix}`;
+
+  const existingRecord = await findUnique({ where: { slug } });
+
+  if (existingRecord) return findNextUniqueSlug(findUnique, baseSlug, suffix + 1);
+  if (suffix >= 500) throw new Error('Tried 500 times, no slug found.'); // TODO: this will some day throw
+
+  return slug;
+};
+
+// copied from '~/domain/utils/toSlug'
+function toSlug(str: string) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  replacements.forEach(([from, to]) => {
+    str = str.replaceAll(from, to);
+  });
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-') // collapse dashes
+    .replace(/^-+|-+$/g, ''); // remove leading and trailing dashes
+
+  return str;
+}
+
+const replacements = [
+  ['à', 'a'],
+  ['á', 'a'],
+  ['ä', 'ae'],
+  ['â', 'a'],
+  ['è', 'e'],
+  ['é', 'e'],
+  ['ë', 'e'],
+  ['ê', 'e'],
+  ['ì', 'i'],
+  ['í', 'i'],
+  ['ï', 'i'],
+  ['î', 'i'],
+  ['ò', 'o'],
+  ['ó', 'o'],
+  ['ö', 'oe'],
+  ['ô', 'o'],
+  ['ù', 'u'],
+  ['ú', 'u'],
+  ['ü', 'ue'],
+  ['û', 'u'],
+  ['ñ', 'n'],
+  ['ç', 'c'],
+  ['·', '-'],
+  ['/', '-'],
+  ['_', '-'],
+  [',', '-'],
+  [':', '-'],
+  [';', '-'],
+];
 
 main();
